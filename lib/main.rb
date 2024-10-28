71534bb772387dfba5be88909ab245f575ac8408
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-28 14:39:44 -0700

    Load Dictionary and select secret word

---
+++ lib/main.rb
@@ -0,0 +1,15 @@
+def create_dictionary(file)
+  File.readlines(file).each_with_object([]) do |word, dictionary|
+    dictionary << word if word.length > 5 && word.length <= 12
+  end
+end
+
+# dictionary = create_dictionary('google-10000-english-no-swears.txt')
+
+# secret_word = dictionary.sample
+
+# puts secret_word
+
+File.readlines('google-10000-english-no-swears.txt').each do |word|
+  p word if word.include?('_')
+end
